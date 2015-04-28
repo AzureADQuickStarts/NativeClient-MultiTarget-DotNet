499fe5fc45cd4f0b014e10c5480b19966aa753a7
dstrockis <dastrock@microsoft.com>
2015-04-28 09:29:44 -0700

    Initial.

---
+++ DirSearchClient-Universal.Windows/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DirectorySearcherLib;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DirSearchClient_Universal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            List<User> results = new List<User>();
+            results.Add(new User());
+            SearchResults.ItemsSource = results;
+
+        }
+
+        private async void Search(object sender, RoutedEventArgs e)
+        {
+            await UnivDirectoryHelper.Search(sender, e, SearchResults, SearchTermText, StatusResult, new PlatformParameters(PromptBehavior.Auto, false));
+        }
+    }
+}
