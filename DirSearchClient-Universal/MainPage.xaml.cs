0c839b595f18c8367083e431da18a9d343e983c1
dstrockis <dastrock@microsoft.com>
2016-03-22 14:52:33 -0700

    Excess code removal

--- DirSearchClient-Universal/MainPage.xaml.cs
+++ DirSearchClient-Universal/MainPage.xaml.cs
@@ -35,22 +35,6 @@ namespace DirSearchClient_Universal
             SearchResults.ItemsSource = results;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-        }
-
         private async void Search(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(SearchTermText.Text))
